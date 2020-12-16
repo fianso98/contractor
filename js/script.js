9d5ec429d4f7f000902441e82d5d3402b6a0ce42
sofiane <sofiane_afir@live.fr>
2020-12-16 12:11:35 +0100

    fixed some design on the menu (js) and changed colors

--- js/script.js
+++ js/script.js
@@ -2,48 +2,48 @@ const brand =document.getElementById('menuBrand');
 const home =document.getElementById('menuHome');
 const work =document.getElementById('menuWork');
 const contact =document.getElementById('menuContact');
+const menu =document.getElementById('Menu');
 
-document.addEventListener('click',handleClick);
-
-function handleClick(e){
-    
-    switch(e.target.id){
-        case 'menuBrand': 
-            home.classList.add('active');
-            work.classList.remove('active');
-            contact.classList.remove('active');;
-        break;
-        case home.id:
-             home.classList.add('active');
-             work.classList.remove('active');
-             contact.classList.remove('active');
-        break;
-        case work.id:
+home.addEventListener('click',()=>{
+    home.classList.add('active');
+    work.classList.remove('active');
+    contact.classList.remove('active');
+    menu.classList.remove('activeBackground');
+});
+work.addEventListener('click',()=>{
             work.classList.add('active');
             contact.classList.remove('active');
             home.classList.remove('active');
-        break;
-        case contact.id: 
-            contact.classList.add('active');
-            home.classList.remove('active');
-            work.classList.remove('active');
-        break;
-        default:null 
-    } 
-}
-
+            menu.classList.add('activeBackground');
+});
+contact.addEventListener('click',()=>{
+    
+    contact.classList.add('active');
+    home.classList.remove('active');
+    work.classList.remove('active');
+    menu.classList.add('activeBackground');
+});
+brand.addEventListener('click',()=>{
+    home.classList.add('active');
+    work.classList.remove('active');
+    contact.classList.remove('active');
+    menu.classList.remove('activeBackground');
+});
 
 
 //second part on scroll
-const menu =document.getElementById('Menu');
+
 
 
 var timer = null;
 window.addEventListener('scroll', function() {
     if(timer !== null) {
         clearTimeout(timer);   
-           
-        menu.classList.add('hide')
+        if(window.scrollY==0){
+            menu.classList.remove('activeBackground');
+           }else{
+        menu.classList.add('activeBackground');
+        menu.classList.add('hide')}
     }
     timer = setTimeout(function() {
         menu.classList.remove('hide')  
